4333ba5d533b3b572521e6d136c4696ec187711e
NewAndycul96 <andycul96@yahoo.com>
2018-11-09 16:07:24 -0600

    finished

--- AudioRecording/AudioViewController.swift
+++ AudioRecording/AudioViewController.swift
@@ -9,87 +9,83 @@
 import UIKit
 import AVKit
 
-class AudioViewController: UIViewController, AVAudioRecorderDelegate {
+class AudioViewController: UIViewController, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
 
-    @IBOutlet weak var playButton: UIBarButtonItem!
-    
-    var audioSession:AVAudioSession!
+    var audioSession: AVAudioSession!
     var audioRecorder: AVAudioRecorder!
+    var isAudioRecorder: Bool!
 
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        audioSession = AVAudioSession.sharedInstance()
-        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]?) -> Bool {
-            let audioSession = AVAudioSession.sharedInstance()
-            do {
-                try audioSession.setCategory(AVAudioSession.Category.playback, mode: .default)
-            } catch {
-                print("Setting category to AVAudioSessionCategoryPlayback failed.")
+        switch AVAudioSession.sharedInstance().recordPermission {
+        case AVAudioSession.RecordPermission.granted:
+            isAudioRecorder = true
+            break
+        case AVAudioSession.RecordPermission.denied:
+            isAudioRecorder = false
+            break
+        case AVAudioSession.RecordPermission.undetermined:
+            AVAudioSession.sharedInstance().recordPermission { [unowned self] allowed in DispatchQueue.main.async {
+                if allowed {
+                    self.isAudioRecorder = true
+                } else {
+                    self.isAudioRecorder = false
+                }
             }
-            // Other project setup
-            return true
         }
-        let recordingSettings =
-            [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
-             AVEncoderBitRateKey: 16,
-             AVNumberOfChannelsKey: 2,
-             AVSampleRateKey: 44100.0] as [String : Any]
-    }
-    let fileManager = FileManager.default
-    let documentDirectoryPaths = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
-    let documentDirectoryURL = documentDirectoryPaths[0]
-
-    let audioFileName = "audio.caf"
-    let audioFileURL = documentDirectoryURL.appendingPathComponent(audioFileName)
-    
-    playButton.image = UIImage(named: "play")
-    let playImage = UIImage(named: "play")
-    
-    playButton.image = playImage
-    
-    func startRecording() {
-        let audioSession = AVAudioSession.sharedInstance()
-        do {
-            audioRecorder = try AVAudioRecorder(url: self.directoryURL()! as URL,
-                                                settings: settings)
-            audioRecorder.delegate = self
-            audioRecorder.prepareToRecord()
-        } catch {
-            finishRecording(success: false)
-        }
-        do {
-            try audioSession.setActive(true)
-            audioRecorder.record()
-        } catch {
+        break
+        default:
+            break
         }
     }
     
-    func finishRecording(success: Bool) {
+    @IBAction func audioRecorder(_ sender: Any) {
+        
+        if isAudioRecorder {
+            
+            let session = AVAudioSession.sharedInstance()
+            
+            do {
+                try session.setCategory(AVAudioSession.Category, mode:.default)
+                try session.setActive(true, options: AVAudioSession.SetActiveOptions)
+                
+                let recordingSettings =
+                    [AVEncoderAudioQualityKey: AVAudioQuality.min.rawValue,
+                     AVEncoderBitRateKey: 16,
+                     AVNumberOfChannelsKey: 2,
+                     AVSampleRateKey: 44100.0] as [String : Any]
+                
+            } catch let error {
+                print("Error for start audio recording")
+            }
+        }
+    }
+    
+    @IBAction func finsihRecording(_ sender: Any) {
+        
+        finishAudioRecording(success: true)
+    }
+    func finishAudioRecording(success: Bool) {
         audioRecorder.stop()
+        audioRecorder = nil
+        
         if success {
-            print(success)
+            print("Recording finished")
         } else {
-            audioRecorder = nil
-            print("Somthing Wrong.")
+            print("Recording failed")
         }
     }
-
-    @IBAction func clickAudioRecord(_ sender: Any) {
-        if audioRecorder == nil {
-            self.clickAudioRecord.setTitle("Stop", for: UIControl.State.normal)
-            self.clickAudioRecord.backgroundColor = UIColor(red: 119.0/255.0, green: 119.0/255.0, blue: 119.0/255.0, alpha: 1.0)
-            self.startRecording()
-        } else {
-            self.clickAudioRecord.setTitle("Record", for: UIControl.State.normal)
-            self.clickAudioRecord.backgroundColor = UIColor(red: 221.0/255.0, green: 27.0/255.0, blue: 50.0/255.0, alpha: 1.0)
-            self.finishRecording(success: true)
-        }
+    func getdocumentsDirectory() -> URL {
+        
+        let fileManager = FileManager.default
+        let documentDirectoryPaths = fileManager.urls(for: .documentDirectory, in: .userDomainMask)
+        let documentDirectoryURL = documentDirectoryPaths[0]
     }
     func audioRecorderDidFinishRecording(_ recorder: AVAudioRecorder, successfully flag: Bool) {
         if !flag {
-            finishRecording(success: false)
+            finishAudioRecording(success: false)
         }
     }
 }
